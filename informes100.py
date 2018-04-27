30136f8a4814b86400f29adb92c904809c037ce8
rampillas <andresrgutierrez@gmail.com>
2018-04-27 15:46:50 +0200

    script diagramas acabado

--- informes100.py
+++ informes100.py
@@ -20,15 +20,25 @@ import matplotlib.pyplot as plt
 from shutil import copyfile
 from pdfkit import from_file as create_pdf
 import contextlib
+from copy import deepcopy
+import matplotlib.pyplot as plt
 
 # necesitamos traer de la db los datos de la linea 100
 actual = datetime.now()
-dia_resta = 2
+dia_resta = 3
 un_minuto = timedelta(minutes=1)
-querie = ("SELECT * FROM autobuses.pasos_parada  where linea=100 "
-          "and fecha='{}-{}-{}'order by coche, viaje").format(actual.year,
-                                                              actual.month,
-                                                              actual.day-dia_resta)
+
+tamaño_correcto_tupla_valde_sardi = 9
+tamaño_correcto_tupla_sardi_valde = 8
+
+querie = ("SELECT pp.Coche, pp.Viaje, pp.Parada, COALESCE(ppa.Instante, "
+          "pp.Instante) as Instante FROM autobuses.pasos_parada pp  left join "
+          "autobuses.pasos_parada_ajustada ppa on pp.Coche=ppa.Coche and "
+          "ppa.Viaje=pp.Viaje and ppa.Linea=pp.Linea and ppa.Fecha=pp.fecha "
+          "and pp.Parada=ppa.Parada where pp.linea=100 and pp.fecha='{}-{}-{}' "
+          "order by pp.coche, pp.viaje, pp.Instante").format(actual.year,
+                                                             actual.month,
+                                                             actual.day-dia_resta)
 querie_cabeceras = ("SELECT coche, viaje, Parada, Instante from "
                     "autobuses.pasos_parada_ajustada where linea=100 "
                     "and parada in (512,509,511,516) and fecha='{}-{}-{}' "
@@ -41,6 +51,18 @@ directorio = "informe_lcentral_{}{}{}/".format(actual.year,
 archivo = "{}{}{}".format(actual.year,
                           actual.month,
                           actual.day-dia_resta)
+
+
+def filtro_valdecilla_sardinero(clave_lista,
+                                tamaño_correcto_tupla_valde_sardi=tamaño_correcto_tupla_valde_sardi):
+    return tamaño_correcto_tupla_valde_sardi != len(clave_lista[1])
+
+
+def filtro_sardinero_valdecilla(clave_lista,
+                                tamaño_correcto_tupla_sardi_valde=tamaño_correcto_tupla_sardi_valde):
+    return tamaño_correcto_tupla_sardi_valde != len(clave_lista[1])
+
+
 if os.path.exists(directorio):
     pass
 else:
@@ -56,8 +78,7 @@ with contextlib.closing(MySQLdb.connect(user='root',
     with contextlib.closing(conexion.cursor()) as cursor:
         cursor.execute(querie)
         datos_row = tuple(cursor)
-        cursor.execute(querie_cabeceras)
-        datos_row_cabeceras = tuple(cursor) 
+
 
 Paradas = namedtuple("Paradas", ("Avenida_Valdecilla", "Valdecilla",
                                  "San_Fernando", "Jesus_Monasterio_7",
@@ -76,28 +97,38 @@ paradas = Paradas(Avenida_Valdecilla=512, Valdecilla=509,
                   Vega_Lamera=171, Casimiro_Sainz_15=76,
                   Paseo_Pereda=39, Correos=40, Pza_Ayto=41,
                   San_Fernando_22=43)
+
 paradas_a_sardinero = (paradas.Valdecilla, paradas.San_Fernando,
                        paradas.Jesus_Monasterio_7, paradas.Correos_plaza,
                        paradas.Jardines_Pereda, paradas.Puerto_Chico,
                        paradas.Casimiro_Sainz_6,
                        paradas.Ies_Llamas, paradas.Int_Sardinero)
+
+paradas_a_sardinero_nombres = ("", "Valdecilla",
+                               "San_Fernando", "Jesus_Monasterio_7",
+                               "Correos_plaza", "Jardines_Pereda",
+                               "Puerto_Chico", "Casimiro_Sainz_6", "Ies_Llamas",
+                               "Int_Sardinero")
+paradas_a_valdecilla_nombres = ("","Int_Sardinero_1",
+                                "Vega_Lamera", "Casimiro_Sainz_15",
+                                "Paseo_Pereda", "Correos", "Pza_Ayto",
+                                "San_Fernando_22", "Avenida_Valdecilla")
+
 paradas_a_valdecilla = (paradas.Int_Sardinero_1, paradas.Vega_Lamera,
                         paradas.Casimiro_Sainz_15, paradas.Paseo_Pereda,
                         paradas.Correos, paradas.Pza_Ayto,
                         paradas.San_Fernando_22, paradas.Avenida_Valdecilla)
 
 Columnas = namedtuple("Columnas",
-                      ("linea",
-                       "coche",
+                      ("coche",
                        "viaje",
                        "parada",
-                       "instante",
-                       "nombre"))
-cols = Columnas(*range(6))
+                       "instante"))
+cols = Columnas(*range(4))
 
 viajes_ordenado = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 
-#si la parada es alguna de las de las cabeceras vamos a usar la consulta de la otra db
+# si la parada es alguna de las de las cabeceras vamos a usar la consulta de la otra db
 for coche_viaje, rows in groupby(datos_row, lambda fila: (fila[cols.coche],
                                                           fila[cols.viaje])):
     Sentido = namedtuple(
@@ -106,17 +137,47 @@ for coche_viaje, rows in groupby(datos_row, lambda fila: (fila[cols.coche],
     sentido_viaje = ''
     for fila in rows:
         if fila[cols.parada] == paradas.Valdecilla:
-            viajes_ordenado[coche_viaje[0]][coche_viaje[1]
-                                            ]['Valdecilla_Sardinero'].append(fila[cols.instante])
+            viajes_ordenado[coche_viaje[0]]['Valdecilla_Sardinero'][coche_viaje[1]
+                                                                    ].append(fila[cols.instante])
             sentido_viaje = sentido.Valdecilla_Sardinero
-        elif fila[cols.parada] == paradas.Int_Sardinero:
-            viajes_ordenado[coche_viaje[0]][coche_viaje[1]
-                                            ]['Sardinero_Valdecilla'].append(fila[cols.instante])
+        elif fila[cols.parada] == paradas.Int_Sardinero_1:
+            viajes_ordenado[coche_viaje[0]]['Sardinero_Valdecilla'][coche_viaje[1]
+                                                                    ].append(fila[cols.instante])
             sentido_viaje = sentido.Sardinero_Valdecilla
         elif fila[cols.parada] in paradas_a_sardinero:
-            viajes_ordenado[coche_viaje[0]][coche_viaje[1]
-                                            ]['Valdecilla_Sardinero'].append(fila[cols.instante])
+            viajes_ordenado[coche_viaje[0]]['Valdecilla_Sardinero'][coche_viaje[1]
+                                                                    ].append(fila[cols.instante])
         elif fila[cols.parada] in paradas_a_valdecilla:
-            viajes_ordenado[coche_viaje[0]][coche_viaje[1]
-                                            ]['Sardinero_Valdecilla'].append(fila[cols.instante])
+            viajes_ordenado[coche_viaje[0]]['Sardinero_Valdecilla'][coche_viaje[1]
+                                                                    ].append(fila[cols.instante])
+# filtro para borrar los malos
 
+viajes_ordenado_filtrado = deepcopy(viajes_ordenado)
+for key, d1 in viajes_ordenado.items():
+    for d2 in filter(filtro_valdecilla_sardinero, d1['Valdecilla_Sardinero'].items()):
+        del viajes_ordenado_filtrado[key]['Valdecilla_Sardinero'][d2[0]]
+
+    for d2 in filter(filtro_sardinero_valdecilla, d1['Sardinero_Valdecilla'].items()):
+        del viajes_ordenado_filtrado[key]['Sardinero_Valdecilla'][d2[0]]
+
+# montamos otro diccionario mas cocreto con los distintos df a pintar
+for coche, valores in viajes_ordenado_filtrado.items():
+    for sentido, valores2 in valores.items():
+        tamaño = tamaño_correcto_tupla_valde_sardi if sentido == 'Valdecilla_Sardinero' else tamaño_correcto_tupla_sardi_valde
+        nombres = paradas_a_sardinero_nombres if sentido == 'Valdecilla_Sardinero' else paradas_a_valdecilla_nombres
+        df = pd.DataFrame.from_dict(
+            viajes_ordenado_filtrado[coche][sentido], orient='columns', dtype=None)
+        df['paradas'] = list(range(tamaño))
+        #df['pasos_parada']=df.apply(tuple, axis=1)
+        columnas = list(df.columns.values)
+        ax = df.plot(x=columnas[0], y='paradas',
+                     label='Viaje {}'.format(str(columnas[0])), figsize=(20,10))
+        for viaje in columnas[1:-1]:
+            df.plot(x=viaje, y='paradas', ax=ax,
+                    label='Viaje {}'.format(str(viaje)))
+        ax.set_yticklabels(nombres)
+        ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+        titulo='Coche {} sentido {}'.format(str(coche), sentido)
+        ax.set_title(titulo, color='black')
+        fig = ax.get_figure()
+        fig.savefig(directorio+'coche_{}_sentido_{}.png'.format(str(coche),sentido))
