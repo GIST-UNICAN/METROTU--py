1e9343491f1391f59cd40b4f8b7cad91e230e3fc
rampillas <andresrgutierrez@gmail.com>
2018-05-08 09:36:17 +0200

    informe dia 7

--- filtra_rpt_a_db_desde_datos_reales_con_estimacion.py
+++ filtra_rpt_a_db_desde_datos_reales_con_estimacion.py
@@ -124,5 +124,5 @@ def filtra_excel(
 
 
 filtra_excel(
-    r"D:\Users\Andres\OneDrive\OneDrive - Universidad de Cantabria\Recordar GIST - VARIOS\datos_tus\4vh.rpt"
+    r"D:\Users\Andres\OneDrive\OneDrive - Universidad de Cantabria\Recordar GIST - VARIOS\datos_tus\7vh.rpt"
 )
