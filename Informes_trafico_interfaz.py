7caf498fcfd07f09c1dfd62ed34c26a28a167cb0
rampillas <andresrgutierrez@gmail.com>
2018-06-14 13:00:40 +0200

    cambios informe

--- Informes_trafico_interfaz.py
+++ Informes_trafico_interfaz.py
@@ -46,10 +46,15 @@ label_fechas=("Fecha inicial comparativa","Fecha final comparativa","Fecha inici
 labels_fechas_rellenas=list()
 comparativa = tk.IntVar(value=1)
 fines_semana = tk.IntVar()
+info_intro_espiras='Para obtener datos agregados introducir espiras separadas por comas \n ejemplo 1001,1002... \n para Obtener datos por separado introducir nombre: espira1,espira2; \n ejemplo Paseo Pereda: 1001,1002,1003; San Fernando: 1006; Los Ciruelos:1005,1008'  
+
 
 def mensaje_error(msg):
     messagebox.showerror('ERROR', msg)
 
+def mensaje_info(msg=info_intro_espiras):
+    messagebox.showinfo('INFO', msg)
+    
 def show_calendar(variable_cambiar):
     def getdate():
         list_fechas[variable_cambiar]=cal.selection_get()
@@ -76,15 +81,22 @@ def mostrar_fechas():
             list_botones[j].config(state="normal")
         
     pass
-
+def format_date(dt):
+    return datetime.strftime(dt,'%d-%m-%Y')
 def ejecutar_informe():
     # hay que sacar todos los datos para llamar a la funcion de generar informe
     continuar=True
+    espiras_agrupadas=False
     try:
-        tupla_espiras=tuple(int(x) for x in entrada.get().split(','))
+        tupla_espiras=tuple((x.lstrip(), tuple(int(a) for a in y.split(','))) for x,y in tuple(x.split(':') for x in a))
+        espiras_agrupadas=True
     except Exception:
-        continuar=False
-        mensaje_error('El formato de las espiras no es correcto, por favor introduzca números separados por comas')
+        #si no son varias probamos con que solo quiera una espira sin agregar
+        try:
+            tupla_espiras=tuple(int(x) for x in entrada.get().split(','))
+        except Exception:
+            continuar=False
+            mensaje_error('El formato de las espiras no es correcto, por favor introduzca números separados por comas o con el formato múltiple correcto')
     try:
         titulo_informe=str(titulo.get())
     except Exception:
@@ -109,47 +121,26 @@ def ejecutar_informe():
         
     pass
 
-
-top.columnconfigure((0,1,2,3), weight=1)
-for i in range (0,4):
-    tk.Label(top, text=label_fechas[i]).grid(row=i, column=0, padx=2, pady=5)
-    list_botones.append(tk.Button(top,text='seleccionar', command=lambda i=i: show_calendar(i)))
-    list_botones[i].grid(row=i, column=1)
-    labels_fechas_rellenas.append(tk.Label(top, text=""))
-    labels_fechas_rellenas[i].grid(row=i, column=2, padx=1, pady=5)
-entrada=tk.Entry(top,width=30)
-entrada.grid(row=4, column=1, padx=1, pady=5, columnspan=2)
-tk.Label(top, text="Espiras a comparar (,)").grid(row=4, column=0, padx=1, pady=5)
-
-titulo=tk.Entry(top,width=30)
-titulo.grid(row=5, column=1, padx=1, pady=5, columnspan=2)
-tk.Label(top, text="Título del informe").grid(row=5, column=0, padx=1, pady=5)
-
-
-tk.Checkbutton(top, text='¿Fines de semana?', variable=fines_semana).grid(row=6, column=0, padx=1, pady=5)
-
-tk.Checkbutton(top, text="¿Hacer comparativa?", variable=comparativa, command=mostrar_fechas).grid(row=7, column=0, padx=1, pady=5)
-
-tk.Button(top, text='Ejecutar', command=ejecutar_informe,width=30).grid(row=8, column=0, padx=1, pady=5, columnspan=3)
-
-top.mainloop()
-
-
-
-
-
-
-
-def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
+def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa, espiras_agrupadas):
     # VARIABLES
+    if not espi
+    nombres=list()
+    espiras_dir_valdecilla=list()
+    limite_descarga=list()
+    for elemento in espiras
+    if espiras_agrupadas:
+        nombres,
+        espiras_dir_valdecilla
+        limite_descarga
+    else:
+        espiras
     espiras_dir_valdecilla = espiras
-    print(espiras_dir_valdecilla)
-    print(type(fechas))
-    print((fechas[1]-fechas[0]).days)
     if comparativa:
         limite_descarga = int(len(espiras_dir_valdecilla)*60*24*max((fechas[1]-fechas[0]).days+1,(fechas[3]-fechas[2]).days+1))
     else:
          limite_descarga = int(len(espiras_dir_valdecilla)*60*24*((fechas[1]-fechas[0]).days+1)) #(7500,7500,1500)
+         
+    print(limite_descarga)
     nombres=nombre_titulo
     cuerpo_informe = ''
     
@@ -168,10 +159,11 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
     if comparativa:
         dia_inicio_anterior = fechas[2]
         dia_fin_anterior = fechas[3]
-        descargas_a_realizar = {(dia_inicio, dia_fin): (espiras_dir_valdecilla, limite_descarga, nombres)}
-    else:
         descargas_a_realizar = {(dia_inicio, dia_fin): (espiras_dir_valdecilla, limite_descarga, nombres),
-                                (dia_inicio, dia_fin): (espiras_dir_valdecilla, limite_descarga, nombres),}
+                                (dia_inicio_anterior, dia_fin_anterior): (espiras_dir_valdecilla, limite_descarga, nombres)}
+       
+    else:
+         descargas_a_realizar = {(dia_inicio, dia_fin): (espiras_dir_valdecilla, limite_descarga, nombres)}
     while dia_control <= dia_fin:
         # SE EXCLUYEN LOS FINES DE SEMANA
         if dia_control.weekday() < 5 and dia_control.day not in dias_excluir:
@@ -190,8 +182,6 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
     for dias, valores in descargas_a_realizar.items():
         query_fines_semana=" and weekday(fecha) not  in (6,5)" if fines_semana  else ""
         dia_inicio = dias[0]
-        print(dia_inicio)
-        print(dias)
         dia_fin = dias[1]
         espiras=valores[0]
         limite=valores[1]
@@ -254,7 +244,7 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
             with contextlib.closing(conexion.cursor()) as cursor:
                 exhaust_map(cursor.execute, querie)
                 df=pd.DataFrame(list(cursor), columns=columnas)
-                datos_row[nombre][dia_inicio.year] = df
+                datos_row[nombre][(dia_inicio, dia_fin)] = df
                 print('datos descargados')
 
     # CREAMOS EL DIRECTORIO
@@ -280,7 +270,9 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
         
         ax_int_oc_list = ax_int_oc.ravel() if comparativa else (ax_int_oc,)
         n=0
+        print(len(datos.items()))
         for año, dataframe in datos.items():
+            label = '{} al {}'.format(format_date(año[0]),format_date(año[1]))
             #empezamos con intensidad ocupacion
             dataframe['intensidad']=dataframe['intensidad'].apply(float)
             dataframe['ocupacion']=dataframe['ocupacion'].apply(float)
@@ -290,21 +282,14 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
             ax_int_oc_list[n].grid()
             ax_int_oc_list[n].set_xlabel('Ocupación (%)', fontsize=15)
             ax_int_oc_list[n].set_ylabel('Intensidad (veh/hora)', fontsize=15)
-            ax_int_oc_list[n].set_title(str(año), fontsize=20)
+            ax_int_oc_list[n].set_title(label, fontsize=20)
             n+=1
             #hacemos lo propio con los de intensidad horaria
             pivot_table=pd.pivot_table(dataframe, values = ['intensidad'], index=['hora'], aggfunc=[np.mean])
             pivot_table.reset_index(inplace=True)
             pivot_table.columns=['Hora', 'Intensidad']
-            pivot_table_interpolada=pd.DataFrame()
-            #interrpolamos para suaviozar las lineas
-            
-            horas=tuple(range(0,24))
-            f1=interp1d(horas,pivot_table['Intensidad'],kind='cubic')
-            pivot_table_interpolada['intensidad_itp']=f1(horas)
-            pivot_table_interpolada['Hora']=horas
-            pivot_table_interpolada.index=horas
-            pivot_table_interpolada.plot(y='intensidad_itp',x='Hora', ax=ax_int_hor, label=str(año))
+           
+            pivot_table.plot(y='Intensidad',x='Hora', ax=ax_int_hor, label=label)
             ax_int_hor.set_xlabel('Hora del día', fontsize=15)
             ax_int_hor.set_ylabel('Intensidad (veh/hora)', fontsize=15)
             
@@ -333,7 +318,7 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
                 str(año))
         figura_ruta = directorio + figura_ruta_relativa
         fig_int_hor.savefig(figura_ruta,bbox_inches='tight')
-        titulo = 'Gráfico Intensidad Horaria {} 2017 vs 2018'.format(destino)
+        titulo = 'Gráfico Intensidad Horaria'
         cuerpo_informe = "".join((cuerpo_informe,
                                       textos_html_informe_trafico.apartado_informe.format(
                                           titulo=titulo,
@@ -364,3 +349,35 @@ def informe_espiras(espiras, fechas, nombre_titulo, fines_semana, comparativa):
                                                                          dia_fin.year,
                                                                          dia_fin.month,
                                                                          dia_fin.day))
+top.columnconfigure((0,1,2,3), weight=1)
+for i in range (0,4):
+    tk.Label(top, text=label_fechas[i]).grid(row=i, column=0, padx=2, pady=5)
+    list_botones.append(tk.Button(top,text='seleccionar', command=lambda i=i: show_calendar(i)))
+    list_botones[i].grid(row=i, column=1)
+    labels_fechas_rellenas.append(tk.Label(top, text=""))
+    labels_fechas_rellenas[i].grid(row=i, column=2, padx=1, pady=5)
+entrada=tk.Entry(top,width=30)
+entrada.grid(row=4, column=1, padx=1, pady=5, columnspan=2)
+tk.Label(top, text="Espiras a comparar (,)").grid(row=4, column=0, padx=1, pady=5)
+tk.Button(top,text='info',command=mensaje_info).grid(row=4, column=3, padx=1, pady=5)
+titulo=tk.Entry(top,width=30)
+titulo.grid(row=5, column=1, padx=1, pady=5, columnspan=2)
+tk.Label(top, text="Título del informe").grid(row=5, column=0, padx=1, pady=5)
+
+
+tk.Checkbutton(top, text='¿Fines de semana?', variable=fines_semana).grid(row=6, column=0, padx=1, pady=5)
+
+tk.Checkbutton(top, text="¿Hacer comparativa?", variable=comparativa, command=mostrar_fechas).grid(row=7, column=0, padx=1, pady=5)
+
+tk.Button(top, text='Ejecutar', command=ejecutar_informe,width=30).grid(row=8, column=0, padx=1, pady=5, columnspan=3)
+
+
+top.mainloop()
+
+
+
+
+
+
+
+
